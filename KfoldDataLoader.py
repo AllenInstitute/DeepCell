8667de4d31ae84decfb5730983e28b8e2b819735
Adam Amster <adam.amster@alleninstitute.org>
2020-11-19 09:15:34 -0500

    Tries random erasing

--- KfoldDataLoader.py
+++ KfoldDataLoader.py
@@ -6,7 +6,7 @@ from Subset import Subset
 
 class KfoldDataLoader:
     def __init__(self, train_dataset, y, n_splits, batch_size, shuffle=True, additional_train_transform=None,
-                 random_state=None, crop_to_center=True):
+                 random_state=None, crop_to_center=True, random_erasing=False):
         self.train_dataset = train_dataset
         self.y = y
         self.n_splits = n_splits
@@ -15,13 +15,14 @@ class KfoldDataLoader:
         self.additional_train_transform = additional_train_transform
         self.random_state = random_state
         self.center_crop = crop_to_center
+        self.random_erasing = random_erasing
 
     def run(self):
         skf = StratifiedKFold(n_splits=self.n_splits, shuffle=self.shuffle, random_state=self.random_state)
         for train_index, test_index in skf.split(np.zeros(len(self.train_dataset)), self.y):
             train = Subset(dataset=self.train_dataset, indices=train_index,
                            additional_transform=self.additional_train_transform,
-                           apply_transform=True, center_crop=self.center_crop)
+                           apply_transform=True, center_crop=self.center_crop, apply_random_erasing=self.random_erasing)
             valid = Subset(dataset=self.train_dataset, indices=test_index, apply_transform=True,
                            center_crop=self.center_crop)
             train_loader = DataLoader(train, batch_size=self.batch_size, shuffle=True)
--- Subset.py
+++ Subset.py
@@ -13,7 +13,7 @@ class Subset(Dataset):
         indices (sequence): Indices in the whole set selected for subset
     """
     def __init__(self, dataset: Dataset, indices: Sequence[int], additional_transform=None,
-                 apply_transform=False, center_crop=True) -> None:
+                 apply_transform=False, center_crop=True, apply_random_erasing=False) -> None:
         self.dataset = dataset
         self.indices = indices
         self.apply_transform = apply_transform
@@ -29,6 +29,10 @@ class Subset(Dataset):
             transform = additional_transform + default_transforms
         else:
             transform = default_transforms
+
+        if apply_random_erasing:
+            # needs to come after ToTensor()
+            transform.append(transforms.RandomErasing(scale=(.02, .1)))
 
         transform = transforms.Compose(transform)
 
