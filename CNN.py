5d1ec4ecb7b41f6bd8ddc9d45ad1d0ffca3c549c
Adam Amster <adam.amster@alleninstitute.org>
2020-11-19 22:33:48 -0500

    increase neurons in hidden

--- CNN.py
+++ CNN.py
@@ -14,10 +14,10 @@ class CNN(nn.Module):
             nn.Linear(last_conv_filter_num * 7 * 7, 64),
             nn.ReLU(inplace=True),
             nn.Dropout(p=dropout_prob),
-            nn.Linear(64, 32),
+            nn.Linear(64, 64),
             nn.ReLU(inplace=True),
             nn.Dropout(p=dropout_prob),
-            nn.Linear(32, num_classes),
+            nn.Linear(64, num_classes),
         )
 
     def forward(self, x):
@@ -45,4 +45,6 @@ class CNN(nn.Module):
                     layers += [conv2d, nn.ReLU(inplace=True)]
                 in_channels = v
         return nn.Sequential(*layers)
+
+
 
