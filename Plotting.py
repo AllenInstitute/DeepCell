d7c322ab77074e0ed708674ec755ad4a79781f9c
Adam Amster <adam.amster@alleninstitute.org>
2020-12-19 12:51:02 -0500

    Adds loss plot with early stopping point

--- Plotting.py
+++ Plotting.py
@@ -1,10 +1,32 @@
-import pandas as pd
-import plotly.express as px
+import plotly.graph_objects as go
 
 
-def plot_loss(train_loss, val_loss):
-    train = pd.DataFrame({'loss': train_loss, 'train or val': 'train', 'epoch': range(len(train_loss))})
-    val = pd.DataFrame({'loss': val_loss, 'train or val': 'val', 'epoch': range(len(val_loss))})
-    data = pd.concat([train, val])
-    fig = px.line(data, x='epoch', y='loss', color='train or val', title='Loss')
+def plot_loss(train_loss, val_loss, best_epoch, early_stopping):
+    fig = go.Figure()
+
+    train_loss = train_loss[:best_epoch + early_stopping]
+    val_loss = val_loss[:best_epoch + early_stopping]
+
+    fig.add_trace(go.Scatter(
+        x=list(range(best_epoch + early_stopping)),
+        y=train_loss,
+        name='train'
+    ))
+    fig.add_trace(go.Scatter(
+        x=list(range(best_epoch + early_stopping)),
+        y=val_loss,
+        name='val'
+    ))
+
+    y0 = min(train_loss[best_epoch], val_loss[best_epoch])
+    y1 = max(train_loss[best_epoch], val_loss[best_epoch])
+
+    fig.add_shape(type="line",
+                  x0=best_epoch, y0=y0 - .05, x1=best_epoch, y1=y1 + .05,
+                  line=dict(
+                      color="red",
+                      width=4,
+                      dash="dot"
+                  )
+                  )
     return fig
