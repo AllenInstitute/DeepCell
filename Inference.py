05f10869592ab34f664c96abcc3024618a62281c
Adam Amster <adam.amster@alleninstitute.org>
2021-10-28 12:00:26 -0400

    try add tta

--- Inference.py
+++ Inference.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import numpy as np
 import os
 import pandas as pd
@@ -9,12 +11,44 @@ from Metrics import Metrics
 from RoiDataset import RoiDataset
 
 
-def inference(model: torch.nn.Module, test_loader: DataLoader, checkpoint_path, has_labels=True, threshold=0.5, ensemble=True,
-              cv_fold=None, use_cuda=True):
+def inference(model: torch.nn.Module, test_loader: DataLoader, checkpoint_path,
+              has_labels=True, threshold=0.5, ensemble=True,
+              cv_fold=None, use_cuda=True,
+              tta_num_iters=0) -> Tuple[Metrics, pd.DataFrame]:
+    """
+    Args:
+        model:
+            torch.nn.Module
+        test_loader:
+            Dataloader
+        checkpoint_path:
+            Path to model checkpoints
+        has_labels:
+            Whether test dataset has labels
+        threshold:
+            Probability threshold for classification
+        ensemble:
+            Whether checkpoint_path points to an ensemble of checkpoints to use
+            If so, inference will be made once per model and averaged
+        cv_fold:
+            Whether to use a specific model cv_fold from checkpoint_path
+        use_cuda:
+            Should be True if on GPU
+        tta_num_iters:
+            Number of times to perform test-time augmentation (default 0)
+
+    Returns:
+        Tuple of Metrics object and dataframe of predictions
+    """
     if not ensemble and cv_fold is None:
         raise ValueError('If not using ensemble, need to give cv_fold')
     if cv_fold is not None and ensemble:
         raise ValueError('Ensemble should be false if passing in cv_fold')
+
+    if tta_num_iters == 1:
+        raise ValueError('test_time_augmentation_num_iters == 1 does not '
+                         'make sense. Either use 0 to disable or a number > '
+                         '1.')
 
     dataset: RoiDataset = test_loader.dataset
     metrics = Metrics()
@@ -25,8 +59,10 @@ def inference(model: torch.nn.Module, test_loader: DataLoader, checkpoint_path, 
     else:
         models = [f'{cv_fold}_model.pt']
 
-    y_scores = np.zeros((len(models), len(dataset)))
-    y_preds = np.zeros((len(models), len(dataset)))
+    num_iters = 1 if tta_num_iters == 0 else tta_num_iters
+
+    y_scores = np.zeros((len(models), len(dataset)), num_iters)
+    y_preds = np.zeros((len(models), len(dataset)), num_iters)
 
     for i, model_checkpoint in enumerate(models):
         state_dict = torch.load(f'{checkpoint_path}/{model_checkpoint}')
@@ -35,24 +71,26 @@ def inference(model: torch.nn.Module, test_loader: DataLoader, checkpoint_path, 
         model.eval()
         prev_start = 0
 
-        for data, _ in test_loader:
-            if use_cuda:
-                data = data.cuda()
+        for iter in range(num_iters):
+            for data, _ in test_loader:
+                if use_cuda:
+                    data = data.cuda()
 
-            with torch.no_grad():
-                output = model(data)
-                output = output.squeeze()
-                y_score = torch.sigmoid(output).cpu().numpy()
-                start = prev_start
-                end = start + data.shape[0]
-                y_scores[i][start:end] = y_score
-                y_preds[i][start:end] = y_score > threshold
-                prev_start = end
+                with torch.no_grad():
+                    output = model(data)
+                    output = output.squeeze()
+                    y_score = torch.sigmoid(output).cpu().numpy()
+                    start = prev_start
+                    end = start + data.shape[0]
+                    y_scores[i][start:end][iter] = y_score
+                    prev_start = end
+
+        y_preds_model = y_preds.mean(axis=-1) > threshold
 
         if has_labels:
-            TP = ((dataset.y == 1) & (y_preds[i] == 1)).sum().item()
-            FP = ((dataset.y == 0) & (y_preds[i] == 1)).sum().item()
-            FN = ((dataset.y == 1) & (y_preds[i] == 0)).sum().item()
+            TP = ((dataset.y == 1) & (y_preds_model == 1)).sum().item()
+            FP = ((dataset.y == 0) & (y_preds_model == 1)).sum().item()
+            FN = ((dataset.y == 1) & (y_preds_model == 0)).sum().item()
             p = TP / (TP + FP)
             r = TP / (TP + FN)
             f1 = 2 * p * r / (p + r)
@@ -60,7 +98,12 @@ def inference(model: torch.nn.Module, test_loader: DataLoader, checkpoint_path, 
             print(f'{model_checkpoint} recall: {r}')
             print(f'{model_checkpoint} f1: {f1}')
 
+    # average over num_iters
+    y_scores = y_scores.mean(axis=-1)
+
+    # average over len(models)
     y_scores = y_scores.mean(axis=0)
+
     y_preds = y_scores > threshold
     if has_labels:
         metrics.update_accuracies(y_true=dataset.y, y_score=y_scores, threshold=threshold)
