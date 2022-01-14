c50831782ca0c7764c107a1575fd64a6d184a8f6
Adam Amster <adam.amster@alleninstitute.org>
2022-01-13 19:17:52 -0500

    add debugging

--- src/deepcell/trainer.py
+++ src/deepcell/trainer.py
@@ -284,18 +284,25 @@ class Trainer:
                        additional_metrics=additional_metrics)
 
     def _reset(self):
+        logger.info(f'gpu mem before reset: {torch.cuda.memory_allocated()}')
         # reset model weights
         x = torch.load(f'{self.save_path}/model_init.pt')
+        logger.info(f'gpu mem after loading checkpoint:'
+                    f' {torch.cuda.memory_allocated()}')
+
         self.model.load_state_dict(x['state_dict'])
-        if torch.cuda.is_available():
-            torch.cuda.empty_cache()
+        logger.info(f'gpu mem after loading state dict for model:'
+                    f' {torch.cuda.memory_allocated()}')
 
         # reset optimizer
         self.optimizer.load_state_dict(state_dict=x['optimizer'])
-
+        logger.info(f'gpu mem after loading state dict for opt:'
+                    f' {torch.cuda.memory_allocated()}')
         # reset scheduler
         if self.scheduler is not None:
             self.scheduler.load_state_dict(state_dict=x['scheduler'])
+            logger.info(f'gpu mem after loading state dict for scheduler:'
+                        f' {torch.cuda.memory_allocated()}')
 
     def _save_model_and_performance(self, eval_fold: int,
                                     all_train_metrics: TrainingMetrics,
