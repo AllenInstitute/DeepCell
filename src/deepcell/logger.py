8bc9754caa9faee455d7a3d3f600625329c0e8d9
Adam Amster <adam.amster@alleninstitute.org>
2022-03-22 11:53:11 -0400

    remove StreamHandler, since it is logging multiple times

--- src/deepcell/logger.py
+++ src/deepcell/logger.py
@@ -1,13 +1,7 @@
 import logging
-import sys
 
 
 def init_logger(name):
     logger = logging.getLogger(name)
-    streamHandler = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - '
-                                  '%(message)s')
-    streamHandler.setFormatter(formatter)
-    logger.addHandler(streamHandler)
     logger.setLevel(logging.INFO)
     return logger
